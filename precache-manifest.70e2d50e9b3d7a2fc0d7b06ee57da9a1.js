8540f7b843f880cd88d77a64b0bd0484f2793bd5
JamesSleep <tyuyiy@naver.com>
2020-03-10 11:30:55 +0900

    Updates

---
+++ precache-manifest.70e2d50e9b3d7a2fc0d7b06ee57da9a1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "447d18b9ce48fdf0ac3c850aa3526af0",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "ce726822eeecba15c118",
+    "url": "/movie_app_2020/static/css/main.2d73a079.chunk.css"
+  },
+  {
+    "revision": "1a493a7e6887c34131e8",
+    "url": "/movie_app_2020/static/js/2.262108bd.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/movie_app_2020/static/js/2.262108bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce726822eeecba15c118",
+    "url": "/movie_app_2020/static/js/main.58f178a7.chunk.js"
+  },
+  {
+    "revision": "f4b1b4d8e3c6be481ba3",
+    "url": "/movie_app_2020/static/js/runtime-main.c0081f24.js"
+  }
+]);
